889ac20bec859f6768408a7473898dce1a42996a
Felobateer <felobateersamy01@gmail.com>
2025-06-29 21:53:16 +0200

    fix build errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,43 +10,32 @@ import LoadingScreen from "./components/loadingScreen";
 
 export default function ScreenTabs() {
   const [tab, setTab] = useState("list");
-  // const [appointments, setAppointments] = useState([]);
-
-  // useEffect(() => {
-  //   const fetchAppointments = async () => {
-  //     const res = await fetch("/api/appointments", {
-  //       method: "GET",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //       },
-  //     });
-  //     const data = await res.json();
-  //     setAppointments(data);
-  //   };
-
-  //   fetchAppointments();
-  // }, []);
-
   const fetcher = (url: string) => fetch(url).then((res) => res.json());
   const { data, error, isLoading } = useSWR("/api/appointments", fetcher);
 
   return (
     <>
-      <Navbar tab={tab} setTab={setTab} />
       {isLoading ? (
         <LoadingScreen />
+      ) : error ? (
+        <p className="text-red-500 text-center">Fehler beim Laden.</p>
+      ) : Array.isArray(data) ? (
+        <>
+          <Navbar tab={tab} setTab={setTab} />
+          <Tabs value={tab} onValueChange={setTab}>
+            <TabsContent value="list">
+              <ListTab meetings={data} />
+            </TabsContent>
+            <TabsContent value="week">
+              <WeekTab meetings={data} />
+            </TabsContent>
+            <TabsContent value="month">
+              <MonthTab meetings={data} />
+            </TabsContent>
+          </Tabs>
+        </>
       ) : (
-        <Tabs value={tab} onValueChange={setTab}>
-          <TabsContent value="list">
-            <ListTab meetings={data} />
-          </TabsContent>
-          <TabsContent value="week">
-            <WeekTab meetings={data} />
-          </TabsContent>
-          <TabsContent value="month">
-            <MonthTab meetings={data} />
-          </TabsContent>
-        </Tabs>
+        <div></div>
       )}
     </>
   );
